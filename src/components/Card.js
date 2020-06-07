1d19b0be4033a48ca8fa64a71d211824d58d7bd5
Kai Wu Toh <kwtoh2000@hotmail.com>
2020-06-07 18:15:31 +0800

    Changed Cards to Buttons and renamed package

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Card = ({ name, id }) => {
 	return (
-		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+		<button className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<img alt='pokemon' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} />
 			<div>
 				<h2>{name}</h2>
 			</div>
-		</div>
+		</button>
 	);
 }
 
