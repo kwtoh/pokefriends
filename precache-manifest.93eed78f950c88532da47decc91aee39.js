4402432d69879e4d71362137e1f3b9013f410994
Kai Wu Toh <kwtoh2000@hotmail.com>
2020-06-07 17:54:52 +0800

    Updates

---
+++ precache-manifest.93eed78f950c88532da47decc91aee39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "efbeef04818e8c6b7f33",
+    "url": "/robofriend/static/css/main.69e3ea4b.chunk.css"
+  },
+  {
+    "revision": "efbeef04818e8c6b7f33",
+    "url": "/robofriend/static/js/main.efbeef04.chunk.js"
+  },
+  {
+    "revision": "4aa9871aaa7f832f2b6b",
+    "url": "/robofriend/static/js/runtime~main.4aa9871a.js"
+  },
+  {
+    "revision": "2763de9438c1a8787115",
+    "url": "/robofriend/static/css/2.04c8a129.chunk.css"
+  },
+  {
+    "revision": "2763de9438c1a8787115",
+    "url": "/robofriend/static/js/2.2763de94.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "2d675fb9703373a332fd2e75305cefb4",
+    "url": "/robofriend/index.html"
+  }
+];
