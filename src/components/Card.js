3432d9a242d09cd488a5c2213b04562cb3ac0ccb
Kai Wu Toh <kwtoh2000@hotmail.com>
2020-06-07 18:46:55 +0800

    Converted Card to Component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Card = ({ name, id }) => {
+class Card extends Component {
+
+
+onClick = (event) => {
+	// Play pokemon cries
+}
+
+render() {
 	return (
-		<button className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='pokemon' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} />
+		<button className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' onclick={this.onClick}>
+			<img alt='pokemon' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`} />
 			<div>
-				<h2>{name}</h2>
+				<h2>{this.props.name}</h2>
 			</div>
 		</button>
-	);
+	); 
+}
+
 }
 
 export default Card;
